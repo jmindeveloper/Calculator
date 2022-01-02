067ff95999e09fca7f805f4f6ddb64850c32c586
jmindevelper <jmin.developer@gmail.com>
2022-01-02 15:49:50 +0900

    영업일 기준 최근환율 가져오기 구현

--- Calculator/Calculator/TypeCalculator/ExchangeRateViewController.swift
+++ Calculator/Calculator/TypeCalculator/ExchangeRateViewController.swift
@@ -13,6 +13,8 @@ class ExchangeRateViewController: UIViewController {
     var rateInformation: [RateInformation] = []
     var beforeCountry = ""
     var afterCountry = ""
+    let dateFormatter = DateFormatter()
+    var currentDateString = ""
     
     @IBOutlet weak var beforeCountryBtnTxt: CustomUIButton!
     @IBOutlet weak var afterCountryBtnTxt: CustomUIButton!
@@ -26,12 +28,22 @@ class ExchangeRateViewController: UIViewController {
         self.setNavigationBar()
         
         // MARK: 환율정보 알림
-        getRate("20211231")
-        let alert = UIAlertController(title: nil, message: "해당 환율 정보는 한국수출입은행에서 제공합니다.\n비영업일 혹은 영업당일 11시 이전에 조회하실경우\n이전영업일의 환율정보로 조회됩니다.", preferredStyle: .alert)
+        
+//        getRate("20211231")
+        
+        let alert = UIAlertController(title: "환율정보 가져오는중", message: "해당 환율 정보는 한국수출입은행에서 제공합니다.\n비영업일 혹은 영업당일 11시 이전에 조회하실경우\n이전영업일의 환율정보로 조회됩니다.", preferredStyle: .alert)
         let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
         alert.addAction(okAction)
-        self.present(alert, animated: true) {
-            self.getRate("20211231")
+        self.present(alert, animated: true) { [weak self] in
+            guard let self = self else { return }
+            var currentDate = ""
+            DispatchQueue.global().sync {
+                currentDate = self.dateformat()
+            }
+            print("sex")
+            self.getRate(currentDate)
+            
+            //            self.getRate("20211231")
             print(self.rateInformation)
         }
         
@@ -43,6 +55,58 @@ class ExchangeRateViewController: UIViewController {
         changeRateCalculatorKeyboard.changeRateDelegate = self
     }
     
+    // MARK: 날짜 구하기
+    func dateformat() -> String {
+        
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        dateFormatter.dateFormat = "yyyy"
+        let year = dateFormatter.string(from: Date())
+        dateFormatter.dateFormat = "MM"
+        let month = dateFormatter.string(from: Date())
+        dateFormatter.dateFormat = "dd"
+        let day = dateFormatter.string(from: Date())
+        dateFormatter.dateFormat = "HH"
+        let hour = dateFormatter.string(from: Date())
+        
+        var currentDayOfWeek = "오류"
+        var currentDate = "오류"
+        var count = 0
+        
+        while currentDayOfWeek == "토" || currentDayOfWeek == "일" || currentDayOfWeek == "오류" || hour < "11" {
+            let beforeDay = Int(day)!
+            let month = Int(month)!
+            let date = String(Int(year + String(format: "%02d", month) + String(format: "%02d", beforeDay))! - count)
+            currentDayOfWeek = businessDate(date).0
+            currentDate = businessDate(date).1
+            count += 1
+        }
+        print("currentDate --> \(currentDate)")
+        return currentDate
+    }
+    
+    func businessDate(_ date: String) -> (String, String) {
+        
+        let weekArray = ["일", "월", "화", "수", "목", "금", "토"]
+        
+        let currentDate = date
+        print("currentDate --> \(currentDate)")
+        
+        let date = DateFormatter()
+        date.locale = Locale(identifier: "ko_kr")
+        date.dateFormat = "yyyyMMdd"
+        
+        guard let inputDay = date.date(from: currentDate) else { print("오류"); return ("오류", "오류") }
+        let cal = Calendar(identifier: .gregorian)
+        let comps = cal.dateComponents([.weekday], from: inputDay)
+        let currentDayOfWeek = weekArray[comps.weekday! - 1]
+        
+        
+        print(currentDayOfWeek)
+        
+        //        return String(year) + String(month) + String(day)
+        return (currentDayOfWeek, currentDate)
+    }
+    
     func setNavigationBar() {
         self.navigationItem.title = "환율계산기"
         
@@ -51,6 +115,7 @@ class ExchangeRateViewController: UIViewController {
         self.navigationItem.leftBarButtonItem = item
     }
     
+    // MARK: 환율 가져오기
     func getRate(_ date: String) {
         guard let url = URL(string: "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=1pXO1xLeYHOZdZEwpdVIvcZ39rfv6Bii&searchdate=\(date)&data=AP01") else { return }
         let session = URLSession(configuration: .default)
@@ -60,11 +125,11 @@ class ExchangeRateViewController: UIViewController {
             guard let rateInformation = try? decoder.decode(Rate.self, from: data) else { return }
             DispatchQueue.main.async {
                 self?.rateInformation = rateInformation
+                print(rateInformation)
             }
         }.resume()
     }
     
-    // MARK: 환율 가져오기
     @objc func showSideMenu() {
         guard let sideMenuVC = self.storyboard?.instantiateViewController(withIdentifier: "SideMenuNavigationController") else { return }
         self.present(sideMenuVC, animated: true, completion: nil)
